e63d9df6606bed0e97a5bf45f0606d8ef5909ebd
Robert Sörensen <robert.sorensen44@gmail.com>
2021-02-18 13:20:00 +0100

    får upp 10 random recept i konsollen. Få ut medDOM

--- script/main.js
+++ script/main.js
@@ -1,12 +1,22 @@
 let content = document.querySelector(".maincontent");
 
 window.onload = function () {
-  //window onload skapa lite recept alternativ? typ populära recept.
-  for (let i = 0; i < 10; i++) {
-    let recipes = document.createElement("div");
-
-    recipes.id = i;
-
-    content.appendChild(recipes);
-  }
+  let random = "random";
+  getApi(random);
 };
+function getApi(getString) {
+  fetch(
+    "https://api.spoonacular.com/recipes/" +
+      getString +
+      "?number=10&apiKey=94564cc1b6754402a0363870a50ee0bf"
+  )
+    .then((response) => response.json())
+
+    .then((data) => {
+        let recipe = []
+      for (let index = 0; index < data.recipes.length; index++) {
+        recipe[index] = data.recipes[index].title;
+        console.log(recipe[index]);
+      }
+    });
+}
