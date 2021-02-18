abe6f66704dfad74646f5e0722294d5b34e352d1
Robert Sörensen <robert.sorensen44@gmail.com>
2021-02-18 14:07:28 +0100

    får upp 12 random recept onload

--- script/main.js
+++ script/main.js
@@ -8,15 +8,37 @@ function getApi(getString) {
   fetch(
     "https://api.spoonacular.com/recipes/" +
       getString +
-      "?number=10&apiKey=94564cc1b6754402a0363870a50ee0bf"
+      "?number=12&apiKey=94564cc1b6754402a0363870a50ee0bf"
   )
     .then((response) => response.json())
 
     .then((data) => {
-        let recipe = []
+      let recipe = [];
       for (let index = 0; index < data.recipes.length; index++) {
-        recipe[index] = data.recipes[index].title;
+        recipe[index] = {
+          Name: data.recipes[index].title,
+          Image: data.recipes[index].image,
+          Time: data.recipes[index].readyInMinutes,
+          Dishtype: data.recipes[index].dishTypes,
+        };
         console.log(recipe[index]);
+
+        let recipeDiv = document.createElement("div");
+        let recipeName = document.createElement("h5");
+        let recipeImage = document.createElement("img");
+        let recipeTime = document.createElement("p");
+        let recipeType = document.createElement("p");
+
+        recipeImage.src = recipe[index].Image;
+        recipeType.innerHTML = "Type of meal: <br>" + recipe[index].Dishtype;
+        recipeTime.innerHTML ="Time: <br>Ready in " + recipe[index].Time + " minutes";
+        recipeName.innerHTML = recipe[index].Name;
+
+        recipeDiv.appendChild(recipeImage);
+        recipeDiv.appendChild(recipeName);
+        recipeDiv.appendChild(recipeType);
+        recipeDiv.appendChild(recipeTime);
+        content.appendChild(recipeDiv);
       }
     });
 }
