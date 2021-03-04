4428860710a1b650afc566d2c48fded1b8a6fefe
Robert Sörensen <robert.sorensen44@gmail.com>
2021-03-04 09:32:26 +0100

    tog bort för random. då vi bytt till complexSearch

--- script/writeout.js
+++ script/writeout.js
@@ -2,108 +2,55 @@ let content = document.querySelector(".maincontent");
 let ingredientsContent = document.querySelector(".ingredients");
 function writeOut(data) {
   let recipe = [];
+  
+  for (let index = 0; index < data.results.length; index++) {
+    recipe[index] = {
+      Name: data.results[index].title,
+      Image: data.results[index].image,
+      Id: data.results[index].id,
+      Time: data.results[index].readyInMinutes,
+      Dishtype: data.results[index].dishTypes,
+      Summary: data.results[index].summary,
+    };
+    console.log(data.results[index]);
 
-  try {
-    for (let index = 0; index < data.recipes.length; index++) {
-      recipe[index] = {
-        Name: data.recipes[index].title,
-        Image: data.recipes[index].image,
-        Id: data.recipes[index].id,
-        Time: data.recipes[index].readyInMinutes,
-        Dishtype: data.recipes[index].dishTypes,
-        Summary: data.recipes[index].summary,
-      };
+    let recipeDiv = document.createElement("div");
+    let imgDiv = document.createElement("div");
+    let recipeInfoDiv = document.createElement("div");
+    let recipeName = document.createElement("h5");
+    let recipeImage = document.createElement("img");
+    let recipeTime = document.createElement("p");
+    let recipeType = document.createElement("p");
+    let recipeSum = document.createElement("p");
+    let toRecipe = document.createElement("button");
 
-      let recipeDiv = document.createElement("div");
-      let imgDiv = document.createElement("div");
-      let recipeInfoDiv = document.createElement("div");
-      let recipeName = document.createElement("h5");
-      let recipeImage = document.createElement("img");
-      let recipeTime = document.createElement("p");
-      let recipeType = document.createElement("p");
-      let recipeSum = document.createElement("p");
-      let toRecipe = document.createElement("button");
+    recipeSum.innerHTML = recipe[index].Summary;
+    toRecipe.innerHTML = "View";
+    toRecipe.className = "recipebtn";
+    toRecipe.id = recipe[index].Id;
+    recipeDiv.id = "recipediv";
+    recipeInfoDiv.id = "info";
+    imgDiv.id = "imgdiv";
+    recipeImage.src = recipe[index].Image;
+    recipeType.innerHTML = "<b>Type of meal:</b> <br>" + recipe[index].Dishtype;
+    recipeTime.innerHTML =
+      "<b>Time:</b> <br>Ready in " + recipe[index].Time + " minutes";
+    recipeName.innerHTML = recipe[index].Name;
 
-      recipeSum.innerHTML = recipe[index].Summary;
-      toRecipe.innerHTML = "View";
-      toRecipe.className = "recipebtn";
-      toRecipe.id = recipe[index].Id;
-      recipeDiv.id = "recipediv";
-      recipeInfoDiv.id = "info";
-      imgDiv.id = "imgdiv";
-      recipeImage.src = recipe[index].Image;
-      recipeType.innerHTML =
-        "<b>Type of meal:</b> <br>" + recipe[index].Dishtype;
-      recipeTime.innerHTML =
-        "<b>Time:</b> <br>Ready in " + recipe[index].Time + " minutes";
-      recipeName.innerHTML = recipe[index].Name;
+    imgDiv.appendChild(recipeImage);
+    imgDiv.appendChild(recipeType);
+    imgDiv.appendChild(recipeTime);
+    imgDiv.appendChild(toRecipe);
 
-      imgDiv.appendChild(recipeImage);
-      imgDiv.appendChild(recipeType);
-      imgDiv.appendChild(recipeTime);
-      imgDiv.appendChild(toRecipe);
+    recipeInfoDiv.appendChild(recipeName);
+    recipeInfoDiv.appendChild(recipeSum);
 
-      recipeInfoDiv.appendChild(recipeName);
-      recipeInfoDiv.appendChild(recipeSum);
+    recipeDiv.appendChild(imgDiv);
+    recipeDiv.appendChild(recipeInfoDiv);
 
-      recipeDiv.appendChild(imgDiv);
-      recipeDiv.appendChild(recipeInfoDiv);
+    content.appendChild(recipeDiv);
 
-      content.appendChild(recipeDiv);
-
-      //viewRecipe();
-    }
-  } catch {
-    for (let index = 0; index < data.results.length; index++) {
-      recipe[index] = {
-        Name: data.results[index].title,
-        Image: data.results[index].image,
-        Id: data.results[index].id,
-        Time: data.results[index].readyInMinutes,
-        Dishtype: data.results[index].dishTypes,
-        Summary: data.results[index].summary,
-      };
-      console.log(data.results[index]);
-
-      let recipeDiv = document.createElement("div");
-      let imgDiv = document.createElement("div");
-      let recipeInfoDiv = document.createElement("div");
-      let recipeName = document.createElement("h5");
-      let recipeImage = document.createElement("img");
-      let recipeTime = document.createElement("p");
-      let recipeType = document.createElement("p");
-      let recipeSum = document.createElement("p");
-      let toRecipe = document.createElement("button");
-
-      recipeSum.innerHTML = recipe[index].Summary;
-      toRecipe.innerHTML = "View";
-      toRecipe.className = "recipebtn";
-      toRecipe.id = recipe[index].Id;
-      recipeDiv.id = "recipediv";
-      recipeInfoDiv.id = "info";
-      imgDiv.id = "imgdiv";
-      recipeImage.src = recipe[index].Image;
-      recipeType.innerHTML =
-        "<b>Type of meal:</b> <br>" + recipe[index].Dishtype;
-      recipeTime.innerHTML =
-        "<b>Time:</b> <br>Ready in " + recipe[index].Time + " minutes";
-      recipeName.innerHTML = recipe[index].Name;
-
-      imgDiv.appendChild(recipeImage);
-      imgDiv.appendChild(recipeType);
-      imgDiv.appendChild(recipeTime);
-      imgDiv.appendChild(toRecipe);
-
-      recipeInfoDiv.appendChild(recipeName);
-      recipeInfoDiv.appendChild(recipeSum);
-
-      recipeDiv.appendChild(imgDiv);
-      recipeDiv.appendChild(recipeInfoDiv);
-
-      content.appendChild(recipeDiv);
-
-      viewRecipe(recipe[index].Id);
-    }
+    viewRecipe(recipe[index].Id);
   }
 }
 
